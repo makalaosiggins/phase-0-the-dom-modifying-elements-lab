cc12fe8df4925fd582042425b27a0a4396561a89
makalaosiggins <wrxmakala@icloud.com>
2024-05-15 07:35:41 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Remove the main node with id "main"
+const main = document.querySelector("#main");
+main.remove();
+
+// Create a new h1 node with id "victory" and set its text content
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME with your desired text
+document.body.appendChild(newHeader);
